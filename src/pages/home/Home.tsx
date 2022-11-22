88b4dc09c9ef3d7f3769848ec989008c01b6071d
navigaid <navigaid@gmail.com>
2022-11-23 07:31:51 +0800

    add button

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,13 @@ import deploy from "gboard/dist/deploy.json";
 import { metaWasm } from "gboard/dist/index";
 import "./Home.css";
 
+import {
+  decodeAddress,
+  GearApi,
+  GearKeyring,
+  getWasmMetadata,
+} from "@gear-js/api";
+
 function stateString(state: boolean): string {
   return state ? "on" : "off";
 }
@@ -11,6 +18,7 @@ function stateString(state: boolean): string {
 function Home() {
   let { api, isApiReady } = useApi();
   let [state, setState] = useState("unknown");
+  let [disabled, setDisabled] = useState(true);
 
   useEffect(() => {
     if (!isApiReady) return;
@@ -49,14 +57,64 @@ function Home() {
             let currentState = stateString((result.toHuman() as any).StatusOf);
             console.log("setting state to:", currentState);
             setState(currentState);
+            setDisabled(false);
           }
         },
       );
     }
-    init().then(sub);
+    init().then(sub).then(() => setDisabled(false));
   }, [isApiReady]);
 
-  return <div className="home">Current State: {state}</div>;
+  let toggle = async () => {
+    const meta = await getWasmMetadata(Buffer.from(metaWasm));
+    const payload = { Toggle: null };
+    const alice = await GearKeyring.fromSuri("//Alice");
+    const aliceHex = decodeAddress(alice.address);
+
+    const gas = await api.program.calculateGas.handle(
+      aliceHex,
+      deploy.programId as `0x${string}`,
+      payload,
+      0,
+      true,
+      meta,
+    );
+    console.log(`GasLimit: ${gas}\n`);
+
+    const msg = {
+      destination: deploy.programId as `0x${string}`,
+      payload,
+      gasLimit: gas.min_limit,
+      value: 0,
+    };
+
+    console.log(msg);
+
+    const tx = api.message.send(msg, meta);
+
+    setDisabled(true);
+    await new Promise((resolve, reject) => {
+      tx.signAndSend(alice, ({ events, status }) => {
+        console.log(`STATUS: ${status.toString()}`);
+        if (status.isFinalized) {
+          resolve(status.asFinalized);
+        }
+        events.forEach(({ event }) => {
+          if (event.method === "ExtrinsicFailed") {
+            reject(api.getExtrinsicFailedError(event).docs.join("\n"));
+          }
+        });
+      });
+    });
+  };
+
+  return (
+    <div className="home">
+      <button onClick={toggle} disabled={disabled}>
+        Current State: {state}
+      </button>
+    </div>
+  );
 }
 
 export { Home };
